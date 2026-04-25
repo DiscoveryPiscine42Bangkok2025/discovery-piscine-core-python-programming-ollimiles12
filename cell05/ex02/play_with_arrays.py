3bd4ce0965c0b96ba9282ff1f9584b06557b9799
ollimiles12 <kakarot.khun@gmail.com>
2026-04-25 14:40:03 +0700

    Add files via upload

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,5 +1,6 @@
 numbers = [2, 8, 9, 48, 8, 22, -12, 2]
-two = [x + 2 for x in numbers if x >5]
+z = numbers[0]
+two = [x + z for x in numbers if x >5]
 
 print(numbers)
 print(two)
--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,5 +1,6 @@
 numbers = [2, 8, 9, 48, 8, 22, -12, 2]
-two = [x + 2 for x in numbers if x > 5]
+z = numbers[0]
+two = [x + z for x in numbers if x > 5]
 my_list = list(set(two))
 print(numbers)
 print(my_list)
