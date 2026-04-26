fceeafeb8dd93b11e88d8e589f60ff7cd0acdef6
ollimiles12 <kakarot.khun@gmail.com>
2026-04-26 12:29:37 +0700

    Add files via upload

---
+++ rush/ex00/checkmate.py
@@ -0,0 +1,49 @@
+def checkmate(p):
+    #split part
+    rows = p.split('\n')
+    grid = [list(row) for row in rows] 
+
+    king = None
+    queens = []
+    bishops = []
+    pawns = []
+    rooks = []
+
+    for x in range(len(grid)):        
+        for y in range(len(grid[x])): 
+            sector_value = grid[x][y]
+            if "R" in sector_value:
+                rooks.append((x,y))
+            elif "P" in sector_value:
+                pawns.append((x,y))
+            elif "B" in sector_value:
+                bishops.append((x,y))
+            elif "Q"in sector_value:
+                queens.append((x,y))
+            elif "K" in sector_value:
+                king = (x,y)
+
+    #analyzed
+    for px, py in pawns:
+        if (px - 1, py - 1) == king or (px - 1, py + 1) == king:
+            print("Success")
+            return
+
+    for bx, by in bishops:
+        for i in range(3):
+            if (bx - i, by - i) == king or (bx - i, by + i) == king or (bx + i, by - i) == king or (bx + i, by + i) == king:
+                print("Success")
+                return
+
+    for rx, ry in rooks:
+        for i in range(3):
+            if (rx, ry - i) == king or (rx, ry + i) == king or (rx - i, ry) == king or (rx + i, ry) == king:
+                print("Success")
+                return
+    
+    for qx,qy in queens:
+        for i in range(3):
+            if (rx, ry - i) == king or (rx, ry + i) == king or (rx - i, ry) == king or (rx + i, ry) == king or (bx - i, by - i) == king or (bx - i, by + i) == king or (bx + i, by - i) == king or (bx + i, by + i) == king:
+                print("Success")
+                return
+    print("No checkmate")
---
+++ rush/ex01/main.py
@@ -0,0 +1,24 @@
+from checkmate import checkmate
+import sys
+def main():
+    args = sys.argv[1:] 
+    for board_file in args:
+        try:
+            with open(board_file, 'r') as file:
+                text = file.read()
+                
+            targets = {".", "Q", "B", "R", "P", "K", "q", "b", "r", "p", "k"}
+
+            count = sum(1 for char in text if char in targets)
+            
+            if count == 16:
+
+                checkmate(text)
+            else:
+                print("Error")
+                
+        except FileNotFoundError:
+            print("Error")
+
+if __name__ == "__main__":
+    main()
